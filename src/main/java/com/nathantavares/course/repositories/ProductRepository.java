44dac7dec0c6790f844d2c827874930e44f7573c
EzioKenway1 <nathan48silva@gmail.com>
2022-08-21 21:36:07 -0300

    Product

---
+++ src/main/java/com/nathantavares/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.nathantavares.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nathantavares.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	
+	
+}
---
+++ src/main/java/com/nathantavares/course/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.nathantavares.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nathantavares.course.entities.Product;
+import com.nathantavares.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
