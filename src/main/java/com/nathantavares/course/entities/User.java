5ba0d1112614c510fd691c7c40db53156e190180
EzioKenway1 <nathan48silva@gmail.com>
2022-08-22 21:08:03 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/nathantavares/course/entities/User.java
+++ src/main/java/com/nathantavares/course/entities/User.java
@@ -12,7 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
